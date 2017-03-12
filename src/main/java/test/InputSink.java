3eb5f6f6f8ccefe03a541c77c6b6fe2e10feebe9
ninja-inc <takiida0605@gmail.com>
2017-03-12 18:28:06 +0900

    first commit

---
+++ src/main/java/test/InputSink.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.integration.annotation.ServiceActivator;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@EnableBinding(Sink.class)
+public class InputSink {
+	@ServiceActivator(inputChannel=Sink.INPUT)
+	public void loggerSink(Object payload) {
+		log.info("Received: " + payload);
+	}
+}
